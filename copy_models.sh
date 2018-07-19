8c513091e106a656def32a3a420d925bff93c8ae
xju <xiangyang.ju@gmail.com>
2018-07-19 15:31:01 -0700

    up

--- copy_models.sh
+++ copy_models.sh
@@ -4,7 +4,7 @@ afix="determine"
 #scp electra.lbl.gov:/home/xju/code/trackingML/ten_hists_normed.npy input/.
 #scp electra.lbl.gov:/home/xju/code/trackingML/loss_trainGauss.pkl loss_trainGauss_${afix}.pkl
 
-scp electra.lbl.gov:/home/xju/code/trackingML/output/data/train_data_uID.pkl.gz output/data/
-afix="RNNModule_1"
+#scp electra.lbl.gov:/home/xju/code/trackingML/output/data/train_data_uID.pkl.gz output/data/
+afix="RNNModule_9"
 scp electra.lbl.gov:/home/xju/code/trackingML/output/model/$afix output/model/
 scp electra.lbl.gov:/home/xju/code/trackingML/output/loss/$afix output/loss/
